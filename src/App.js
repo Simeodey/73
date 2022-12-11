c25a86d42b1a40bdd19d7737ab3925ef0e6b1e93
Simeodey <adamand12@abv.bg>
2022-12-11 19:37:54 +0200

    dsa

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(1)
+  useEffect(()=>{
+    document.title = `Count (${count})`;
+  },[count]);
   return (
     <div className="App">
       <section class="hero">
         <div class="hero-body">
-          <p class="title">A React Task</p>
+          <p class="title"></p>
           <p class="subtitle">by Boom.dev</p>
         </div>
       </section>
       <div class="container is-fullhd">
         <div class="notification">
-          Edit the <code>./src</code> folder to add components.
+          <button onClick={()=>{setCount(prev=>++prev)}}>{`Count (${count})`}</button>
         </div>
       </div>
     </div>
